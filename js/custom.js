fa5566334fecb136a902e352aafbb86719b576bc
Dat Ho <datht2311@gmail.com>
2024-04-30 09:59:45 +0700

    Fix the span of "what people say"

--- js/custom.js
+++ js/custom.js
@@ -106,12 +106,11 @@
     });
     
     
-
     $('.certifications h4').on('click', function() {
         $(this).next('p').slideToggle(); // Toggles the visibility of the paragraphs under each header
     });
 
-    $('.testimonials h3').on('click', function() {
+    $('.testimonials h4').on('click', function() {
         $(this).next('p').slideToggle(); // Toggles the visibility of the paragraphs under each header
     });
 
